3247fcb6d0a76cc3d33b0a761e04cd3ecedc4258
Rannie Ollit <einnar82@gmail.com>
2023-04-25 11:11:00 +0800

    UI fixes

--- admin/src/layouts/default/Dashboard.vue
+++ admin/src/layouts/default/Dashboard.vue
@@ -29,7 +29,7 @@ const logout = async () => {
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Administrator</v-toolbar-title>
+      <v-toolbar-title style="cursor: pointer">Administrator</v-toolbar-title>
       <v-spacer></v-spacer>
 
       <v-btn prepend-icon="mdi-logout"
